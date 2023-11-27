5dc6fe49e80e1e737f2b852a06be80756a8a3a11
Adrielly Camargo <adriiellycamargo@gmail.com>
2023-11-26 21:05:40 -0300

    exibição das tarefas na pagina home concluido

--- index.js
+++ index.js
@@ -49,9 +49,8 @@ app.get('/', (requisicao, resposta) => {
             }
         })
         
+        resposta.render('home', {tarefas})
     })
-
-    resposta.render('home')
 })
 
 const conexao = mysql.createConnection({
