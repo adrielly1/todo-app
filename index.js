669361bf0d2e61c402347a6595c216067a8c4fd7
Adrielly Camargo <adriiellycamargo@gmail.com>
2023-11-26 23:02:40 -0300

    Filtrando as tarefas ativas

--- index.js
+++ index.js
@@ -35,6 +35,24 @@ app.post('/completar', (requisicao, resposta) => {
     })
 })
 
+app.post('/descompletar', (requisicao, resposta) =>{
+    const id = requisicao.body.id
+
+    const sql = `
+        UPDATE tarefas
+        SET completa = '0'
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.post('/criar', (requisicao, resposta) => {
     const descricao = requisicao.body.descricao
     const completa = 0
@@ -68,8 +86,14 @@ app.get('/', (requisicao, resposta) => {
                 completa: dado.completa === 0 ? false : true
             }
         })
+
+        const tarefasAtivas = tarefas.filter((tarefa) =>{
+            return tarefa.completa === false && tarefa
+        })
+
+        const quantidadeTarefasAtivas = tarefasAtivas.length
         
-        resposta.render('home', {tarefas})
+        resposta.render('home', {tarefas, quantidadeTarefasAtivas})
     })
 })
 
